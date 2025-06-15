657d4b8e155f3a20b102f7cca8a8ea0201e53af4
FuegoAlecs <fuegoalecs@gmail.com>
2025-06-15 16:55:18 +0100

    Update useAlchemy.ts

--- src/hooks/useAlchemy.ts
+++ src/hooks/useAlchemy.ts
@@ -52,7 +52,7 @@ interface TopTradingPairData {
   pairUrl: string;
 }
 
-const FALLBACK_ALCHEMY_API_KEY_PLACEHOLDER = "OA8inGxL7DDI-mzrnnBi8";
+const FALLBACK_ALCHEMY_API_KEY_PLACEHOLDER = "jFa3wNWqfvKYb9GrCUtmk";
 
 export const useAlchemy = () => {
   const [isLoading, setIsLoading] = useState(false);
